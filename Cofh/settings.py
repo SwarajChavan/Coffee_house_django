a2af65eea20dd77c74e4d1509ac07e13cb61cf0c
SwarajChavan <swarajchavan55@gmail.com>
2021-07-25 02:25:42 +0530

    corrections

--- Cofh/settings.py
+++ Cofh/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
